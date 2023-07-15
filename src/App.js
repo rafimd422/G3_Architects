a432d78dc30a028cf8c7c1e5fb8224fb2f7f71e1
rafimd422 <rafimf2222@gmail.com>
2023-07-16 00:02:59 +0600

    Updated responsiveness of the webpage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import './index.css';
-import Header from './Componant/header';
-import Features from './Componant/features';
-import OtherFeatures from './Componant/otherFeatures';
+  import './App.css';
+  import './index.css';
+  import Header from './Componant/header';
+  import Features from './Componant/features';
+  import OtherFeatures from './Componant/otherFeatures';
 
 
-function App() {
+  function App() {
   return (
-<>
-<Header />
-<Features />
-<OtherFeatures versionOne = "Dexktop & Mobile Version" versionTwo = "Drag & Drop Builder" versionThree = "Super Easy to Edit" versionFour = "Dexktop & Mobile Version"/>
-</>
+  <>
+  <Header />
+  <Features />
+  <OtherFeatures versionOne = "Dexktop & Mobile Version" versionTwo = "Drag & Drop Builder" versionThree = "Super Easy to Edit"     versionFour = "Dexktop & Mobile Version"/>
+  </>
   );
 }
 
-export default App;
+  export default App;
--- src/Componant/otherFeatures.jsx
+++ src/Componant/otherFeatures.jsx
@@ -5,8 +5,8 @@ const OtherFeatures = (props) => {
 
     const {versionOne, versionTwo, versionThree, versionFour} = props;
   return (
-<section className="moreFeatures">
-    <div className="description">
+    <section className="moreFeatures">
+        <div className="description">
         <div className="featureTitle">
             <div className="div1"></div>
             <h2>Features you will <br /> love & enjoy</h2>
@@ -14,7 +14,7 @@ const OtherFeatures = (props) => {
 <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even</p>
    
 
-    <div className="descBox">
+     <div className="descBox">
         <h3>{versionOne}</h3>
         <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised...........</p>
     </div>
@@ -29,16 +29,17 @@ const OtherFeatures = (props) => {
     <div className="descBox">
         <h3>{versionFour}</h3>
         <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised...........</p>
-    </div>
+    </div> 
+
     </div>
 
-<div className="featureImg">
+        <div className="featureImg">
         <button>10+Year <br/>
-       <span>Experience</span> </button>
-    <img src={architect} />
-</div>
-</section>
-  )
-}
+        <span>Experience</span> </button>
+        <img src={architect} />
+        </div>
+    </section>
+        )
+    }
 
-export default OtherFeatures;
+    export default OtherFeatures;
